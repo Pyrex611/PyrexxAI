d0e28aa3ab9a50d8794808db8996324be200356f
Pyrex <gamboclifford11@gmail.com>
2026-06-07 00:48:37 +0100

    Fincaliing re-integrating speed insight and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "sonner";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -307,6 +308,7 @@ export default function RootLayout({
           <AIAssistant />
           {children}
           <Analytics />
+					<SpeedInsights />
           <CookieBanner />
         </ThemeProvider>
       </body>
