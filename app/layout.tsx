eea796888bb7ace9b9e5ccc0dd93b683f1e64f8a
Pyrex <gamboclifford11@gmail.com>
2026-08-06 23:57:29 +0100

    Resolve

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,9 +151,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://cal.com" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }} />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(softwareSchema) }} />
       </head>
